1e2ae5efba4b00dc30ae9bb0dbf1e75384604591
joaopedropinto <32523778+joaopedropinto@users.noreply.github.com>
2024-10-16 16:30:21 -0300

    adding change

--- Cepedi.ProjetoRFID.Leitura.Domain/Services/TagRfidModelService.cs
+++ Cepedi.ProjetoRFID.Leitura.Domain/Services/TagRfidModelService.cs
@@ -6,5 +6,12 @@ namespace Cepedi.ProjetoRFID.Leitura.Domain.Services;
 
 public class TagRfidModelService : BaseService<TagRfidModel>, ITagRfidModelService
 {
-    
+    private readonly ITagRfidModelRepository _TagRfidModelrepository;
+
+    public TagRfidModelService(INotifier notifier, ITagRfidModelRepository TagRfidModelrepository) : base(notifier, TagRfidModelrepository)
+    {
+        _TagRfidModelrepository = TagRfidModelrepository;
+    }
+
+    public async Task
 }
